653b94c89c307729c266a6b490ae92f7d5e74386
Samurai-way <olegmoishevych@gmail.com>
2023-05-08 11:56:41 +0300

    feat: save create app file

---
+++ src/commons/createApp.ts
@@ -0,0 +1,57 @@
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+
+import { AppModule } from '../app.module';
+import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './exceptionFilter';
+import * as cookieParser from 'cookie-parser';
+
+export const createApp = (app: INestApplication): INestApplication => {
+  app.use(cookieParser());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidUnknownValues: false,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+        errors.forEach((e) => {
+          const constraintsKeys = Object.keys(e.constraints);
+          constraintsKeys.forEach((ckey) => {
+            errorsForResponse.push({
+              message: e.constraints[ckey],
+              field: e.property,
+            });
+          });
+        });
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Instagram api')
+    .setDescription('Instagram api')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-Auth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  return app;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,58 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { createApp } from './commons/createApp';
+import { get } from 'http';
+import { createWriteStream } from 'fs';
 
 const PORT = process.env.PORT || 3000;
+const serverUrl = 'http://localhost:3000';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const rawApp = await NestFactory.create(AppModule, {
+    rawBody: true,
+    cors: {
+      origin: ['https://instagram-ui-nine.vercel.app/'],
+      credentials: true,
+    },
+  });
+  const app = createApp(rawApp);
   await app.listen(PORT, () => {
     console.log(`Server started on ${PORT} port`);
   });
+  if (process.env.NODE_ENV === 'development') {
+    console.log('DEVELOP', process.env.NODE_ENV);
+    get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+      console.log(
+        `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+      );
+    });
+    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+      console.log(
+        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+      );
+    });
+    get(
+      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+        );
+        console.log(
+          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+        );
+      },
+    );
+
+    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+      console.log(
+        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+      );
+    });
+  }
 }
 
 bootstrap();
