cdb760f0eee5409cbf890bffebd32a8e5f71414e
Samurai-way <olegmoishevych@gmail.com>
2023-05-11 23:42:57 +0300

    fix: cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,29 @@ import * as process from 'process';
 import { createApp } from './commons/createApp';
 import { get } from 'http';
 import { createWriteStream } from 'fs';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const PORT = process.env.PORT || 3000;
 const serverUrl = 'http://localhost:3000';
 
 async function bootstrap() {
+  const origin = [
+    'https://instagram-ui-nine.vercel.app',
+    'http://localhost:3000',
+    '*',
+  ];
   const rawApp = await NestFactory.create(AppModule, {
     rawBody: true,
     cors: {
-      origin: ['https://instagram-ui-nine.vercel.app', 'http://localhost:3000'],
-      credentials: true,
+      origin,
     },
   });
+  const getCorsOptions = (origin: string[]): CorsOptions => ({
+    origin,
+    credentials: true,
+  });
   const app = createApp(rawApp);
+  app.enableCors(getCorsOptions(origin));
   await app.listen(PORT, () => {
     console.log(`Server started on ${PORT} port`);
   });
