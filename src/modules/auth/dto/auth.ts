b712e266540d7d48bb3b0925631c08c9198da1f4
Samurai-way <olegmoishevych@gmail.com>
2023-05-13 18:49:18 +0300

    feat: create lengthConstants for authDto

--- src/modules/auth/dto/auth.ts
+++ src/modules/auth/dto/auth.ts
@@ -2,26 +2,40 @@ import { Transform, TransformFnParams } from 'class-transformer';
 import { IsEmail, IsString, IsUUID, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
+const lengthConstants = {
+  login: {
+    minLength: 3,
+    maxLength: 10,
+  },
+  password: {
+    minLength: 6,
+    maxLength: 20,
+  },
+};
+
 export class AuthDto {
   @Transform(({ value }: TransformFnParams) => value?.trim())
   @ApiProperty({
     description: 'User login',
     example: 'John',
     type: 'string',
-    minLength: 3,
-    maxLength: 10,
+    minLength: lengthConstants.login.minLength,
+    maxLength: lengthConstants.login.maxLength,
   })
-  @Length(3, 10)
+  @Length(lengthConstants.login.minLength, lengthConstants.login.maxLength)
   login: string;
   @Transform(({ value }: TransformFnParams) => value?.trim())
   @ApiProperty({
     description: 'User password',
     example: 'string',
     type: 'string',
-    minLength: 6,
-    maxLength: 20,
+    minLength: lengthConstants.password.minLength,
+    maxLength: lengthConstants.password.maxLength,
   })
-  @Length(6, 20)
+  @Length(
+    lengthConstants.password.minLength,
+    lengthConstants.password.maxLength,
+  )
   password: string;
   @Transform(({ value }: TransformFnParams) => value?.trim())
   @ApiProperty({
@@ -36,14 +50,17 @@ export class AuthDto {
 
 export class NewPasswordDto {
   @Transform(({ value }: TransformFnParams) => value?.trim())
-  @Length(6, 20)
+  @Length(
+    lengthConstants.password.minLength,
+    lengthConstants.password.maxLength,
+  )
   @ApiProperty({
     description: 'newPassword',
     example: 'qwerty',
     type: 'string',
     format: 'newPassword',
-    minLength: 6,
-    maxLength: 20,
+    minLength: lengthConstants.password.minLength,
+    maxLength: lengthConstants.password.maxLength,
   })
   newPassword: string;
   @Transform(({ value }: TransformFnParams) => value?.trim())
